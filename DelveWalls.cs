977e7fea54c60f030cba3179b5642c49233b6a86
Sirais <github@huede.org>
2019-12-17 16:12:34 +0100

    added initimage to fix filenotfound errors once a while

--- DelveWalls.cs
+++ DelveWalls.cs
@@ -30,6 +30,7 @@ namespace DelveWalls
         public override bool Initialise()
         {
             IngameUI = GameController.IngameState.IngameUi;
+            Graphics.InitImage("directions.png", true);
             return true;
         }
 
